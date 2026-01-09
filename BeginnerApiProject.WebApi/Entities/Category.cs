22047492c37fc0ddcc878686015a796e4e7cb5f9
emirhan-coban <emirhancob4n@gmail.com>
2026-01-09 16:38:41 +0300

    Add project files.

---
+++ BeginnerApiProject.WebApi/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BeginnerApiProject.WebApi.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
